d3b353d58002fe4d1e9dc5f1a4f425cc1f6731d2
Steven <calhoun.12386@gmail.com>
2024-06-27 14:47:55 -0700

    ready to summit

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+const userInputString = prompt(
+  "Please enter some integers separated by commas."
+);
+
+const stringArray = userInputString.split(",");
+
+const list = [];
+
+for (let item of stringArray) {
+  let found = false;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].name === item) {
+      list[i].qty++;
+      found = true;
+    }
+  }
+
+  if (!found) {
+    const order = {
+      name: item,
+      qty: 1,
+    };
+    list.push(order);
+  }
+}
+
+console.log(list);
+console.table(list);
 
